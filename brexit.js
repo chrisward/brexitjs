e096a7eb760493c0859ce507c021b21da2ac4554
chrisward <christopher.j.ward@gmail.com>
2016-07-25 12:24:46 +0100

    Added in realistic delays.

--- brexit.js
+++ brexit.js
@@ -1,3 +1,7 @@
+const ARTICLE_50_DELAY = 63113852000;
+const BRITISH_DAWDLING_DELAY = 31556926000;
 var GB = document.querySelector('script[src$=brexit.js]');
 var EU = GB.parentNode;
-EU.removeChild(GB);
+setTimeout(function() {
+  EU.removeChild(GB);
+}, ARTICLE_50_DELAY + BRITISH_DAWDLING_DELAY);
